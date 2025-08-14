1ef65a009cd0b0bfb0f189c781801d8e9f5151c0
Daniel Wolner <daniel.wolner@gmail.com>
2025-08-13 21:29:41 -0700

    remove react build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { ArrowDown, ArrowUp } from "lucide-react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Bar,
   BarChart,
